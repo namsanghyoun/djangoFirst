c16db60057b765157e9e589a8f195d29eaa443d6
Nam <skatkdgus7@daum.net>
2021-04-19 18:22:54 +0900

    CBV로 포스트 목록 페이지 만들기

--- myApp/urls.py
+++ myApp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.single_post_page),
-    path('', views.index),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('', views.PostList.as_view()),  # CBV 방식
+    # path('<int:pk>/', views.single_post_page), <- FBV 방식 주석처리
+    # path('', views.index), <- FBV 방식 주석처리
 ]
--- myApp/views.py
+++ myApp/views.py
@@ -1,25 +1,36 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
 
-    return render(
-        request,
-        'myApp/index.html',
-        {
-            'posts' : posts,
-        }
-    )
+# CBV 방식의 코드
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
+
+# FBV 방식의 코드
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#
+#     return render(
+#         request,
+#         'myApp/post_list.html',
+#         {
+#             'posts' : posts,
+#         }
+#     )
 
 def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
+     post = Post.objects.get(pk=pk)
 
-    return render(
-        request,
-        'myApp/single_post_page.html',
-        {
-            'post' : post,
+     return render(
+         request,
+         'myApp/single_post_page.html',
+         {
+             'post' : post,
 
-        }
-    )
+         }
+     )
