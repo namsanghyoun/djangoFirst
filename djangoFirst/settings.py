95955f18b3b3cd2601ca28f79590a2de6576e202
Nam <skatkdgus7@daum.net>
2021-04-23 21:41:06 +0900

    포스트 목록 페이지에 대표 이미지 출력하기

--- djangoFirst/settings.py
+++ djangoFirst/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+# 이미지폴더 사용을 위해서 os import
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,6 +123,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
