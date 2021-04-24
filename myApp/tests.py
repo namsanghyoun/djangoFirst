29dce3d26a1a93be8d70772610548f3e712aaa05
Nam <skatkdgus7@daum.net>
2021-04-24 22:49:19 +0900

    네비게이션 바와 푸터 모듈화하기

--- myApp/tests.py
+++ myApp/tests.py
@@ -8,6 +8,23 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client()
 
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        logo_btn = navbar.find('a', text="Nam's Tech & Log")
+        self.assertIn(logo_btn.attrs['href'], '/')
+
+        home_btn = navbar.find('a', text='Home')
+        self.assertEqual(home_btn.attrs['href'], '/')
+
+        blog_btn = navbar.find('a', text='Blog')
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+
+        about_me_btn = navbar.find('a', text='About Me')
+        self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
+
     def test_post_list(self):
         # 1.1 포스트 목록 페이지를 가져온다.
         response = self.client.get('/myApp/')
@@ -17,10 +34,11 @@ class TestView(TestCase):
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'myApp')
         # 1.4 네비게이션 바가 있다.
-        navbar = soup.nav
+        #navbar = soup.nav
         # 1.5 myApp, About Me라는 문구가 네비게이션 바에 있다.
-        self.assertIn('Home', navbar.text)
-        self.assertIn('About', navbar.text)
+        #self.assertIn('Blog', navbar.text)
+        #self.assertIn('About Me', navbar.text)
+        self.navbar_test(soup)
 
         # 2.1 메인 영역에 게시물이 하나도 없다면
         self.assertEqual(Post.objects.count(), 0)
@@ -68,9 +86,10 @@ class TestView(TestCase):
         soup = BeautifulSoup(response.content, 'html.parser')
 
         # 2.2. 포스트 목록 페이지와 똑같은 네비게이션 바가 있다.
-        navbar = soup.nav
-        self.assertIn('Home', navbar.text)
-        self.assertIn('About', navbar.text)
+        #navbar = soup.nav
+        #self.assertIn('Blog', navbar.text)
+        #self.assertIn('About Me', navbar.text)
+        self.navbar_test(soup)
 
         # 2.3. 첫 번째 포스트의 제목이 웹 브라우저 탭 타이틀에 들어 있다.
         self.assertIn(post_001.title, soup.title.text)
