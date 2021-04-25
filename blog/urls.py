82b56dc41af33252a104514f2e880f3927de19ac
Nam <skatkdgus7@daum.net>
2021-04-25 21:41:19 +0900

    카테고리 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('category/<str:slug>/', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),  # CBV 방식
     # path('<int:pk>/', views.single_post_page), <- FBV 방식 주석처리
