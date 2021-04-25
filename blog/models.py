8bed14022f2b39b0e128830da80598dd31e0e787
Nam <skatkdgus7@daum.net>
2021-04-25 18:55:48 +0900

    포스트 목록 페이지에 카테고리 기능 추가하기

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,17 @@ from django.contrib.auth.models import User
 import os
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_test = models.CharField(max_length=100, blank=True)
@@ -16,9 +27,13 @@ class Post(models.Model):
 
     # 작성자 추가
     # CASCADE : 종속의 의미. 작성자가 지워지면 관련글도 삭제됨.
-    #author = models.ForeignKey(User, on_delete=models.CASCADE)
-    # SET_NULL : 작성자가 삭제되었을 때 관련글은 NULL 처리됨.
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # SET_NULL : 작성자가 삭제되었을 때 작성자는 NONE 으로 출력됨. null=True 처리 필수
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    # 카테고리 추가
+    # blank 설정을 통해 필수로 등록해야하는지 체크
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
--- blog/views.py
+++ blog/views.py
@@ -1,35 +1,17 @@
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Category
 
 
-# CBV 방식의 코드
 class PostList(ListView):
     model = Post
     ordering = '-pk'
 
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
+
+
 class PostDetail(DetailView):
     model = Post
-
-# FBV 방식의 코드
-# def index(request):
-#     posts = Post.objects.all().order_by('-pk')
-#
-#     return render(
-#         request,
-#         'blog/post_list.html',
-#         {
-#             'posts' : posts,
-#         }
-#     )
-
-# def single_post_page(request, pk):
-#      post = Post.objects.get(pk=pk)
-#
-#      return render(
-#          request,
-#          'blog/single_post_page.html',
-#          {
-#              'post' : post,
-#
-#          }
-#      )
