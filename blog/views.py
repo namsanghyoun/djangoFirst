cea1072678dbc29341b5a68cfadd2f1265b17343
Nam <skatkdgus7@daum.net>
2021-04-27 21:34:11 +0900

    포스트 작성 페이지 접근 권한 설정하기

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post, Category, Tag
 
 
@@ -25,14 +25,17 @@ class PostDetail(DetailView):
         return context
 
 
-class PostCreate(LoginRequiredMixin, CreateView):
+class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Post
     fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.is_staff
 
     def form_valid(self, form):
         current_user = self.request.user
         # 현재 유저가 로그인한 상태이면
-        if current_user.is_authenicated:
+        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
             # 새로 생성한 포스트의 작성자 필드에 로그인한 사용자를 담는다.
             form.instance.author = current_user
             return super(PostCreate, self).form_valid(form)
