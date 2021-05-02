e3d574ecfa925b75531077b064aeff63960df03a
Nam <skatkdgus7@daum.net>
2021-05-03 00:01:15 +0900

    도커에 장고만 담기

--- djangoFirst/settings.py
+++ djangoFirst/settings.py
@@ -22,12 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=l)!0gtcni)241mo=hm3bn^dpvzwv_c-5^x^@%u&opaqn%tq^$'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'l)!0gtcni)241mo=hm3bn^dpvzwv_c-5^x^@%u&opaqn%tq^$')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', 1))
 
-ALLOWED_HOSTS = []
+if os.environ.get('DJANGO_ALLOWED_HOSTS'):
+    ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+else:
+    ALLOWED_HOSTS = []
 
 
 # Application definition
